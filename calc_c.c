a48229d089a867050175c8fd229a59f676ec91d5
hafiscp <cphafis3@gmail.com>
2020-10-15 01:06:21 +0530

    Trying To Modify 1st

--- calc_c.c
+++ calc_c.c
@@ -1,5 +1,7 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<conio.h>
 int n,a,b,res=0;
+char yes='y';
 int add()
 {
 	res=a+b;
@@ -18,32 +20,38 @@ int div()
 }
 int main()
 {
-	printf("Enter The Mathematical Operation \n");
-	printf("1. Addition \t 2. Subtraction \t 3. Multiplication \t 4. Division \t\n");
-	scanf("%d",&n);
-	printf("Enter The Two Terms :");
-	scanf("%d%d",&a,&b);
-	switch(n)
+	do
 	{
-		case 1:
-			add();
-			printf("Result = %d",res);
-			break;
-		case 2:
-			sub();
-			printf("Result = %d",res);
-			break;
-		case 3:
-			pro();
-			printf("Result = %d",res);
-			break;
-		case 4:
-			div();
-			printf("Result = %d",res);
-			break;
-		default:
-			printf("Enter a given value :\n");
-			break;
-	}
+		printf("\nWant to Try? Y/N \t\n");
+		scanf("%c",&yes);
+		printf("\n\nEnter The Mathematical Operation \n");
+		printf("1. Addition \t 2. Subtraction \t 3. Multiplication \t 4. Division \t\n");
+		scanf("%d",&n);
+		printf("Enter The Two Terms :");
+		scanf("%d%d",&a,&b);
+		switch(n)
+		{
+			case 1:
+				add();
+				printf("Result = %d",res);
+				break;
+			case 2:
+				sub();
+				printf("Result = %d",res);
+				break;
+			case 3:
+				pro();
+				printf("Result = %d",res);
+				break;
+			case 4:
+				div();
+				printf("Result = %d",res);
+				break;
+			default:
+				printf("Enter a given value :\n");
+		}
+		
+	}while(yes=='y' || yes=='Y');
+	main();
 	return 0;
 }
